da4931357b42793d19c7cb24feac1811c1068ea8
ardhikaxx <ardhikayanuar58@gmail.com>
2026-07-06 10:32:40 +0700

    fix: tambah unsafe-eval di CSP untuk development mode

--- middleware.ts
+++ middleware.ts
@@ -8,7 +8,9 @@ export function middleware(req: NextRequest) {
   res.headers.set('Referrer-Policy', 'strict-origin-when-cross-origin');
   res.headers.set(
     'Content-Security-Policy',
-    "default-src 'self'; img-src 'self' data: blob:; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline';"
+    process.env.NODE_ENV === 'development'
+      ? "default-src 'self'; img-src 'self' data: blob:; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline';"
+      : "default-src 'self'; img-src 'self' data: blob:; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline';"
   );
   return res;
 }
