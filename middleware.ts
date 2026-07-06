806144c46d81c250b15b5115bd53174a0bf69407
ardhikaxx <ardhikayanuar58@gmail.com>
2026-07-06 10:15:37 +0700

    feat: penambahan middleware security headers

---
+++ middleware.ts
@@ -0,0 +1,18 @@
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+export function middleware(req: NextRequest) {
+  const res = NextResponse.next();
+  res.headers.set('X-Frame-Options', 'DENY');
+  res.headers.set('X-Content-Type-Options', 'nosniff');
+  res.headers.set('Referrer-Policy', 'strict-origin-when-cross-origin');
+  res.headers.set(
+    'Content-Security-Policy',
+    "default-src 'self'; img-src 'self' data: blob:; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline';"
+  );
+  return res;
+}
+
+export const config = {
+  matcher: '/:path*',
+};
