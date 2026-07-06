5468ee9b2ceae5f618b189815270946a544f40f9
ardhikaxx <ardhikayanuar58@gmail.com>
2026-07-06 10:33:32 +0700

    feat: konfigurasi awal dan halaman utama lengkap dengan Tailwind v4, Next.js 16, dan struktur layout

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,17 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          { key: 'Strict-Transport-Security', value: 'max-age=63072000' },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
